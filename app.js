5de02278c2e2ca7270dddbbdad5c39e617f27b38
rawan <rawanzghoul48@gmail.com>
2020-12-26 21:55:52 +0200

    lab8

--- app.js
+++ app.js
@@ -5,6 +5,17 @@ console.log(name1);
 var time =prompt('time');
 if(time<12){var timestatus ='am';}else {var timestatus='pm';}
 alert (timestatus);
+var age =prompt('your age');
+while (age === ''|| age === null){age ='pleeeeeease your age  for God sake');}
+var x =prompt('how many students do you know studying in HU');
+for (var i = x ;i>0 ;i--){document.write('<img src ="https://tse3.mm.bing.net/th?id=OIP.3ANjjJhiIxab9_QOOcEk6wHaFE&pid=Api&P=0&w=247&h=170">');}
+
+
+
+
+
+ 
+
 
 
 
