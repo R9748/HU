509b9749ef477e9c8265267320cce5752a8c2675
rawan <rawanzghoul48@gmail.com>
2020-12-26 14:38:39 +0200

    lab6

---
+++ app.js
@@ -0,0 +1,10 @@
+var name1 =prompt('your name');
+var message;
+alert('welcome'+ name1);
+console.log(name1);
+var time =prompt('time');
+if(time<12){var timestatus ='am';}else {var timestatus='pm';}
+alert (timestatus);
+
+
+
