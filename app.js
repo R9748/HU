3b4ce406e300a93b8808fd17f6816faf875404c5
rawan <rawanzghoul48@gmail.com>
2020-12-27 23:38:35 +0200

    lab9

--- app.js
+++ app.js
@@ -6,9 +6,11 @@ var time =prompt('time');
 if(time<12){var timestatus ='am';}else {var timestatus='pm';}
 alert (timestatus);
 var age =prompt('your age');
-while (age === ''|| age === null){age ='pleeeeeease your age  for God sake');}
+validation(age);
+
 var x =prompt('how many students do you know studying in HU');
 for (var i = x ;i>0 ;i--){document.write('<img src ="https://tse3.mm.bing.net/th?id=OIP.3ANjjJhiIxab9_QOOcEk6wHaFE&pid=Api&P=0&w=247&h=170">');}
+function validation (age) {while (age === ''|| age === null){age ='pleeeeeease your age  for God sake');}
 
 
 
